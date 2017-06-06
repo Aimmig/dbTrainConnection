33d093dcb9ece2bb10686d2f0c000dae0d438188
Andre Immig <aimmig@students.uni-mainz.de>
2017-06-06 09:11:11 +0200

    Add key event for refreshing for refreshing filter

--- DBFahrplanApi/Python/MainWidget.py
+++ DBFahrplanApi/Python/MainWidget.py
@@ -251,6 +251,9 @@ class FormWidget(qw.QWidget):
     def getConnectionsOnClickInDetails(self):
         #get selected Row in connection details
         row=self.detailsTable.currentRow()
+        #avoid error if nothing is selected
+        if row<0:
+                return
         #get the displayedIndex Information
         (pageIndex,connectionIndexOnPage)=self.conList.getDetailsIndices()
         #select the clicked stop from the displayed Connection (Details)
@@ -271,6 +274,9 @@ class FormWidget(qw.QWidget):
     def getDetails(self):
         #get the selected Index
         index=self.connectionTable.currentRow()
+        #avoid error if nothing was selected
+        if index<0:
+                return
         #get the connection according to the index
         connection=self.conList.getSingleConnection(self.conList.getDisplayedIndex(),index)
         #if stopList is empty request details information
@@ -322,8 +328,10 @@ class FormWidget(qw.QWidget):
               self.setConnectionLabel()      
 
     def refreshPage(self):
-        self.connectionTable.setRowCount(0)
-        self.addConnections(self.conList.getConnectionPage(self.conList.getDisplayedIndex()))
+        index =self.conList.getDisplayedIndex()
+        if index>0:
+                self.connectionTable.setRowCount(0)
+                self.addConnections(self.conList.getConnectionPage(index))
         
     #next navigation
     def showNextPage(self):
@@ -544,5 +552,16 @@ class FormWidget(qw.QWidget):
                 
     #updates and shows settingsWidget
     def showSettingsWidget(self):
-              self.settingsWidget.update()
+              self.settingsWidget.update()          
 
+    #basic keyPressEvent for getting connections
+    def keyPressEvent(self,e):
+        #enter/return to search stations/connections
+        if e.key() == qc.Qt.Key_Return or e.key() == qc.Qt.Key_Enter:
+                if self.stationId==[]:
+                        self.getStations()
+                else:
+                        self.getConnectionsNow()
+        #all other events are passed to the super keyPressEvent
+        else:
+                super(FormWidget,self).keyPressEvent(e)
