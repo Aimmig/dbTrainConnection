f28407866b32a1db566a89b5779bde2838e00c14
Andre Immig <aimmig@students.uni-mainz.de>
2017-06-17 18:20:45 +0200

    Add documentation for Structs

--- DBFahrplanApi/Structs.py
+++ DBFahrplanApi/Structs.py
@@ -23,9 +23,15 @@
 from PyQt5 import QtCore as qc
 from PyQt5 import QtGui as qg
 
-#clas representing a single train connection
-class Connection:
-
+class ConnectionsSettings:
+    """ 
+    Class that holds all relevant constant and fix options
+    for formatting and converting connection to String.
+    Holds base strings for string representation
+    (generall and details) of a connection. 
+    Holds time and date formatting strings.
+    """
+    
     #format date as dd.M.yy
     dateFormat="dd.M.yy"
     #formate time as hh:mm
@@ -36,8 +42,19 @@ class Connection:
     detailsBaseString="Zugverlauf von "
     generallBaseString="Fahrplantabelle für "
     datePrefix=" am "
-        
+
+class Connection:
+    """
+    Class for encapsulation of a single train-connection.
+    """
+    
     def __init__(self,name,typ,stopid,stopName,time,date,direction,origin,track,ref):
+        """
+        Constructs a new new connection from name,type,stopid,
+        stopName,time,date,direction,origin,track and reference link.
+        Initializes empty stop-List and image-Data.
+        """
+        
         #Name e.g. IC10250,ICE516, etc
         self.name=name
         #Typ: IC,ICE, EC,..
@@ -63,32 +80,56 @@ class Connection:
         #construct connection with empyt imageData
         self.imageData=qc.QByteArray()
 
-    #returns properly formatted dateString
     def timeToString(self):
-        return self.time.toString(Connection.timeFormat)
+        """
+        Formats the connection time using Qt.Time method
+        with format specified in ConnectionsSettings.
+        """
+        
+        return self.time.toString(ConnectionsSettings.timeFormat)
+        
+    def dateToString(self):
+        """
+        Formats the connection date using Qt.Date method
+        with format specified in ConnectionsSettings.
+        """
 
-    #string representation of a connection for details label
+        return self.date.toString(ConnectionsSettings.dateFormat)
+        
     def toStringDetails(self):
-        res=Connection.detailsBaseString + self.name + Connection.datePrefix +self.date.toString(Connection.dateFormat)
+        """
+        Returns a detailed string representation of the connection
+        for use in details label.
+        """
+        
+        res=ConnectionsSettings.detailsBaseString + self.name + ConnectionsSettings.datePrefix +self.dateToString()
         return res
 
-    #genereall String represenatation of a connection for overview
     def toStringGenerall(self):
-        res=Connection.generallBaseString + self.stopName
+        """
+        Returns a genereall String represenatation of the connection
+        for overview.
+        """
+        
+        res=ConnectionsSettings.generallBaseString + self.stopName
         if self.origin:
-                res+=Connection.departureString
+                res+=ConnectionsSettings.arrivalString
         else:
-                res+=Connection.arrivalString
-        res+=Connection.datePrefix +self.date.toString(Connection.dateFormat)
+                res+=ConnectionsSettings.departureString
+        res+=ConnectionsSettings.datePrefix +self.dateToString()
         return res
 
-#class representing a single train stop of a connection
 class Stop:
-    
-    #format time with hh:mm
-    timeFormat="hh:mm"
+    """
+    Class for representing a single stop of a train-connection.
+    """
 
     def __init__(self,name,identifier,arrTime,arrDate,depTime,depDate,track,lon,lat):
+        """
+        Constructs stop of name,id, arrivalTime, arrivalDate,
+        departureTime, departureDate, Track, longitude, latitude.
+        """
+        
         #name of the stop-station
         self.name=name
         #id of the stop-station
@@ -103,60 +144,104 @@ class Stop:
         #position of the stop
         self.pos=Coordinate(lon,lat)
 
-    #returns formatted arrTimeDateString
     def arrTimeToString(self):
-        return self.arrTime.toString(Stop.timeFormat)
+        """
+        Formats the stops arrival time using Qt.Time method
+        with format specified in ConnectionsSettings.
+        """
+        
+        return self.arrTime.toString(ConnectionsSettings.timeFormat)
     
-    #returns formatted depTimeString
     def depTimeToString(self):
-        return self.depTime.toString(Stop.timeFormat)
+        """
+        Formats the stops departure time using Qt.Time method
+        with format specified in ConnectionsSettings.
+        """
+        
+        return self.depTime.toString(ConnectionsSettings.timeFormat)
 
-#just for encapsulation of longituted, latitude
 class Coordinate:
+    """
+    Class for representing a geographical coordinate.
+    """
 
-    #construct coordinate with longituted, latitude
     def __init__(self,lon,lat):
+        """
+        Constructs Coordinate from longitude and longitude.
+        """
+        
         self.lon=lon
         self.lat=lat
 
-#class for filtering connections based on train type
 class Filter:
+    """
+    Class for type-based filtering of connections.
+    All type-flags that are set True on construction
+    are types that should be included after filtering.
+    """
     
-    #member variables are true if this type should be included
     def __init__(self,ICE,IC,other):
+        """
+        Constructs a filter with the given bool flags
+        ICE,IC,other.
+        Parameters should be True if the type should be
+        included after filtering, false otherwise.        
+        """
+        
         self.ICE=ICE
         self.IC=IC
         self.other=other
     
-    #static method for filtering trainType ICE
     @staticmethod
     def filterICE(con):
+        """
+        Returns true if the connection con is of type ICE.
+        """
+        
         return con.type=="ICE" or "ICE" in con.name 
     
-    #static method for filtering trainType IC
     @staticmethod
     def filterIC(con):
+        """
+        Returns true if the connection con is of type IC.
+        """
+        
+        #be sure to exclude ICE here
         return con.type==("IC" or "IC" in con.name) and not Filter.filterICE(con)
     
-    #static method for filtering trainType EC
     @staticmethod
     def filterEC(con):
+        """
+        Returns true if the connection con is of type EC.
+        """
+        
         return con.type=="EC" or "EC" in con.name
 
-    #static method for filtering trainType TGV
     @staticmethod
     def filterTGV(con):
+        """
+        Returns true if the connection con is of type TGV.
+        """
+        
         return con.type=="TGV" or "TGV" in con.name
     
-    ##static method for filtering all other train types
     @staticmethod 
     def filterOther(con):
+        """
+        Returns true if the connection is not of type filtered before.
+        """
+        
         #connection has train type other if it does not have an above stated train type
         return not(Filter.filterICE(con) or Filter.filterIC(con) or Filter.filterEC(con) or Filter.filterTGV(con))
-        
-    #filters list of connection based on set train types
-    #returns list of originall indices of all train that pass the filter
+
     def filter(self,connections):
+        """
+        Fiters a list of connections by filtering each connection.
+        Only filters on type if type flag was set.
+        Groups ICE/TGV to ICE, EC/IC to IC.
+        Returns list of original indices of all connections that passed filter.
+        """
+        
         res=[]
         #iterate over all connections in list
         for i in range(len(connections)):
@@ -176,56 +261,101 @@ class Filter:
         #return list of indices
         return res
 
-#class to encapsulate connection data structure and managa indices
 class ConnectionsList:
+    """
+    Class for encapsulation of a list of connections.
+    Holds a list of lists of connections where each list of connections
+    represents one page.
+    Holds the index of the displayed page and a tuple (i,j)
+    so that details of connection j from page i are  displayed.
+    """
 
-    #construct ConnectionList
     def __init__(self):
+        """
+        Constructs a connectionList.
+        Initialize list empty.
+        Set indices to -1.
+        """
+        
         #no information at construction
         self.connectionPages=[]
         #thus indices -1
         self.displayedIndex=-1
         self.displayedDetailedIndex=(-1,-1)
 
-    #returns the single connection with given index on given page
     def getSingleConnection(self,pageIndex,conIndexOnPage):
+        """
+        Returns the conIndexOnPage'th connection on page PageIndex.
+        """
+        
         return self.connectionPages[pageIndex][conIndexOnPage]
 
-    #returns the i-th stop of the single connection with given index on given Page
     def getStop(self,pageIndex,conIndexOnPage,i):
+        """
+        Returns the i-th stop of connection specified by 2 given indices.
+        """
+        
         return self.getSingleConnection(pageIndex,conIndexOnPage).stopList[i]
 
-    #returns the connectionList (page) with given Index
     def getConnectionPage(self,pageIndex):
+        """
+        Returns the pageIndex'th list of connection (page).
+        """
+        
         return self.connectionPages[pageIndex]
         
-    #returns the amount of pages
     def getPageCount(self):
+        """
+        Returns the amount of pages.
+        """
+        
         return len(self.connectionPages)
-     
-    #appends connections as page
+
     def appendPage(self,connections):
+        """
+        Appends a list of connections to the end of connectionPages (new Page).
+        """
+        
         self.connectionPages.append(connections)
 
-    #returns DetailsIndices
     def getDetailsIndices(self):
+        """
+        Returns the index of the connection and the index of the page, 
+        which this connection is on, whos details are currently displayed.
+        """
+        
         return self.displayedDetailedIndex
 
-    #returns DisplayedIndex
     def getDisplayedIndex(self):
+        """
+        Returns the index of the page that is currently displayed.
+        """
+        
         return self.displayedIndex
     
-    #sets Displayed index to given value
     def setDisplayedIndex(self,val):
+        """
+        Sets the index of the page that is currently displayed to given value.
+        """
+        
         self.displayedIndex=val
       
-    #sets DetailIndices to pageIndex and row
     def setDisplayedDetailedIndex(self,pageIndex,row):
+        """
+        Sets index of the connection whos details are currently displayed to row.
+        The connection occurs on page pageIndex in connectionPages.
+        """
+        
         self.displayedDetailedIndex=(pageIndex,row)
-
-#class that encapsulate all important settings for requesting
-#like mapOptions and offset        
+       
 class RequestSettings:
+    """
+    Class that encapsulates Request-settings.
+    Holds Marker size/color, Path size/color and the height/width
+    of the Map that will be requested.
+    Holds default values and a minimum for map size.
+    Holds the offset used when requesting later/earlier.
+    """
 
     #static settings that can not be changed (at the moment)
     MARKER_SIZE='small'
@@ -236,50 +366,80 @@ class RequestSettings:
             
     #initialize changeable settings with default Values
     def __init__(self,defaultSize,defaultOffSet):
+        """
+        Construct RequestSettings with given defaultSize of the map
+        and the default offset used when requesting earlier/later.
+        """
+        
         self.PATH_COLOR=qg.QColor('#ff0000')
         self.MARKER_COLOR=qg.QColor('#5555BB')
         self.height=defaultSize
         self.width=defaultSize
         self.offSet=defaultOffSet
-    
-    #retunrs string repr. of PathColor that can be used in urls
+
     def formatPathColor(self):
+        """
+        Returns string representation of the PathColor that can be used in URL.
+        """
+        
         return self.PATH_COLOR.name().replace('#','0x')
     
-    #retunrs string repr. of MarkerColor that can be used in urls
     def formatColor(self):
+        """
+        Returns string representation of the MarkerColor that can be used in URL.
+        """
+        
         return self.MARKER_COLOR.name().replace('#','0x')
 
-    #retunrs string repr. of MarkerColorSpecial that can be used in urls
     def formatSpecialColor(self):
+        """
+        Returns string representation of the MarkerColorSpecial that can be used in URL.
+        """
+                
         return RequestSettings.MARKER_COLOR_SPECIAL.name().replace('#','0x')
-    
-    #set MarkerColor to given color
+
     def setMarkerColor(self,col):
+        """
+        Set MarkerColor to given color.
+        """
+        
         self.MARKER_COLOR=col
 
-    #set PathColor to given color
     def setPathColor(self,col):
+        """
+        Set PathColor to given color.   
+        """
+        
         self.PATH_COLOR=col
-    
-    #set height to given value
+
     def setHeight(self,h):
+        """
+        Sets height to given value.
+        If below minimum use minimum.
+        """
+        
         #prevent to small size
         if h>=RequestSettings.MIN_SIZE:
                 self.height=h
         else:
                 self.height=RequestSettings.MIN_SIZE
 
-    #set width to given value
     def setWidth(self,w):
+        """
+        Sets width to given value.
+        If below minimum use minimum.
+        """
+        
         #prevent to small size
         if w>=RequestSettings.MIN_SIZE:
                 self.width=w
         else:
                 self.width=RequestSettings.MIN_SIZE
-
-    #set offset to given value
     def setOffSet(self,s):
+        """
+        Sets the offset used when requesting earlier/later to given value.
+        """
+        
         #do not set invalid offsets
         if s<=0:
                 return
